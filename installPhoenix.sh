f303ae342e19f6cc96f6ceea1a7a557702b1db90
Markus Bucher <markusbucher@gmx.de>
2011-07-11 00:52:25 +0200

    [TASK] Adding new functions, arguments, tests

--- installPhoenix.sh
+++ installPhoenix.sh
@@ -5,6 +5,11 @@
 # usage: installPhoenix.sh --ARGUMENT1 --ARGUMENT2 ... 
 #
 
+COMMANDLINE_USER=$(whoami)
+if [ $COMMANDLINE_USER!="root" ]; then
+	echo "You probably want to execute this script as superuser. If you experience errors please execute this script with 'sudo'"
+	echo "e.g. sudo installPhoenix --dbname=phoenix --dbuser=mydbuser --dbpass=keepMeSoSecret"
+fi
 
 ##
 # Set the defaults
@@ -12,12 +17,30 @@
 dbhost=127.0.0.1
 dbname=phoenix
 package=TYPO3v5
+subfolder=TYPO3v5
+
 ##
 # Set the configuration for this script
 
-MANDATORY="dbuser,dbpass,gituser,destination"
+MANDATORY="dbuser,dbpass,gituser"
 OIFS=$IFS
 IFS=,
+
+##
+# Get some system information
+
+mywd=$(pwd)
+mydate=$(date)
+phoenixpath=$mywd/$subfolder
+
+function prepareLogging(){
+	
+	logfile="$mywd/installPhoenix.log"
+	errorlogfile="$mywd/installPhoenixError.log"
+	
+	echo " Script started at $mydate by $COMMANDLINE_USER" >> $logfile
+}
+
 ##
 # Print usage information
 
@@ -32,10 +55,15 @@ Usage: $0 [OPTIONS]
 	--dbuser		Provide name of the database user with write access to dbname
 	--dbpass		Provide password of the database user
 	--gituser		Provide username with access to https://review.typo3.org
-	--destination		Provide a path that acts as the destination for Phoenix
+	--subfolder=TYPO3v5		Provide the name of the subfolder in which Phoenix will be installed
+	--debug			Print debug information and exit
 EOF
 		exit 1
 }
+
+
+##
+# Escape given args
 
 append_arg_to_args () {
   args="$args "`shell_quote_string "$1"`
@@ -43,14 +71,14 @@ append_arg_to_args () {
 
 
 ##
-# process the given arguments, check for mandatory ones
+# Process the given arguments, check for mandatory ones
 
 parse_arguments() {
 	pick_args=
 	if test "$1" = PICK-ARGS-FROM-ARGV
-			then
-				pick_args=1
-				shift
+		then
+			pick_args=1
+			shift
 	fi  
 
 	for arg do
@@ -62,11 +90,20 @@ parse_arguments() {
 			--dbuser=*) dbuser="$val" ;;
 			--dbpass=*) dbpass="$val" ;;
 			--gituser=*) gituser="$val" ;;
-			--destination=*) destination="$val" ;;
+			--subfolder=*) subfolder="$val"; 
+				if [ $subfolder=="" ]; then
+					returnError 6 "--subfolder must not be empty or contain a slash when given"
+				fi
+				echo $mywd
+				#unset phoenixpath; 
+				phoenixpath=$mywd/$subfolder 
+			;;
 
 			--guided) guidedInstallation ;;
 
 			--help) usage ;;
+			
+			--debug) debug ;;
 
 			*)
 				echo "You provided unrecognized arguments."
@@ -83,24 +120,20 @@ parse_arguments() {
 #	dbuser
 #	dbpass
 #	gituser
-#	destination
+#	subfolder
 
 check_requirements () {
 	
 	# Check mandatories
 	for i in $MANDATORY 
 	do
-		if [ -n "${!i}" ] 
+		if [ ! -n "${!i}" ] 
 		then
-			#do some stupid stuff because bash wouldn't like an empty then-statement
-			foo="bar"
-		else
 			errors=$i" $errors"
 		fi
 	done
-	echo $errors
-	if [ "$errors" != '' ]; then
-		returnMissingArgumentError $errors
+	if [ -n "$errors" ]; then
+		returnError 1 $errors
 	fi
 	
 	#check path
@@ -108,15 +141,58 @@ check_requirements () {
 	#check SQL Connection
 }
 
-returnMissingArgumentError() {
+##
+# Prints out an error message
+# arg1 int type of error
+# arg2 string messages
+#
+# 1 = Mandatory field(s) missing
+# 2 = Connection to MySQL not possible
+# 3 = subfolder not empty
+# 4 = Could not connect to git repo
+# 5 = Permission problem
+
+returnError() {
+echo
+echo
+echo "ERROR:"
+echo
+case $1 in 
+1)
 	IFS=" "
 	echo "Some mandatory arguments were not set. Please correct this and start this script again"
 	echo 
-	for singleError in $errors
+	for singleError in $2
 	do
 		echo "Missing: --$singleError "
 	done
 	exit 1
+	;;
+2)
+	echo "Connection to the DB was not possible, please check your credentials."
+	exit 1
+	;;
+3)
+	echo "The subfolder you specified is not empty. Please use another folder or delete the contents of the chosen one."
+	exit 1
+	;;
+4)
+	echo "The git-repository could not be reached due to network problems. Do you have connection to internet?"
+	exit 1
+	;;
+5)
+	echo "You don't have enough permissions to execute this file operation. Please try executing this script with 'sudo'"
+	exit 1
+	;;
+6)
+	echo $2
+	exit 1
+	;;
+*)
+	echo "An error occured, plase have a look at $errorlogfile."
+	exit 1
+	;;
+esac
 }
 
 ##
@@ -131,15 +207,29 @@ shell_quote_string() {
 # Process git clone and register the neccessary submodule
 
 function getPhoenix(){
-	git clone --recursive git://git.typo3.org/TYPO3v5/Distributions/Base.git $destination 
+	git clone --recursive git://git.typo3.org/TYPO3v5/Distributions/Base.git $subfolder 1>>$logfile 2>$errorlogfile
+	if [ $? -gt 0 ]; then
+		returnError 4
+	fi
 
-	cd $destination
+	cd $subfolder
 
 	scp -p -P 29418 $GITUSER@review.typo3.org:hooks/commit-msg .git/hooks/
-
+	if [ $? -gt 0 ]; then
+		returnError 4
+	fi
 	git submodule foreach 'scp -p -P 29418 $GITUSER@review.typo3.org:hooks/commit-msg .git/hooks/'
+	if [ $? -gt 0 ]; then
+		returnError 4
+	fi
 	git submodule foreach 'git config remote.origin.push HEAD:refs/for/master'
+	if [ $? -gt 0 ]; then
+		returnError 4
+	fi
 	git submodule foreach 'git checkout master; git pull'
+	if [ $? -gt 0 ]; then
+		returnError 4
+	fi
 }
 
 function getFLOW3() {
@@ -152,7 +242,7 @@ function getFLOW3() {
 
 function createSettings(){
 
-cd $destination
+cd $subfolder
 SETTINGS=$( cat <<.
 TYPO3: 
 FLOW3: 
@@ -185,16 +275,14 @@ function initializeFLOW3(){
 }
 
 function returnSuccessMessage(){
-	clear
-	echo "Installation of TYPO3 phoenix finished. If you are using xdebug pleas make sure that
+	echo "Installation of TYPO3 phoenix finished. If you are using xdebug please make sure that
 	   xdebug.max_nesting_level is set to a value like 1000 inside php.ini"
 	echo
-	echo "Please have fun with your new system! Inspir people to share"
+	echo "Please have fun with your new system! Inspire people to share"
 	echo
 	echo
 	echo
 }
-
 
 function guidedInstallation(){
 	# Guided goes here
@@ -207,12 +295,31 @@ function guidedInstallation(){
 function doInstall() {
 	getPhoenix
 	createSettings
+	setPermissions
 	initializeFLOW3
 	returnSuccessMessage
 }
 
+function debug(){
+	parse_arguments PICK-ARGS-FROM-ARGV "$@"
+	echo "
+	--package=$package
+	--dbhost=*$dbhost
+	--dbname=$dbname
+	--dbuser=$dbuser
+	--dbpass=$dbpass
+	--gituser=$gituser
+	--subfolder=$subfolder
+	phoenixpath=$phoenixpath
+	"
+	exit 0
+}
+##
+# Beginning the work
 
+prepareLogging
 parse_arguments PICK-ARGS-FROM-ARGV "$@"
 check_requirements
+
 doInstall
 exit 0
